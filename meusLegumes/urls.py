00d7af1617fbf152173c024772cadf122dfeda1b
yago batista da silva <yagobatistasilva@gmail.com>
2017-11-17 12:15:29 -0200

    migrações e arrumando a base.html

--- meusLegumes/urls.py
+++ meusLegumes/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from vendas.view import home
+from vendas import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^',view.home),
+    url(r'^$',views.home),
 ]
