105f0d85e24ba668e12b5766b8a9c0c81954dca0
yago batista da silva <yagobatistasilva@gmail.com>
2017-11-17 12:27:28 -0200

    add produtos e mod base.html

--- vendas/models.py
+++ vendas/models.py
@@ -4,3 +4,19 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Produtos(models.Model):
+    nome = models.CharField(max_length= 32 )
+    imagem = models.ImageField(upload_to='vendas/produtos')
+    valor = models.FloatField(default='None')
+    def __str__(self):
+        return self.nome;
+class Usuario(object):
+    """docstring for ."""
+    telefone = models.CharField(max_length= 11 )
+    rua = models.CharField(max_length= 250 )
+    numero = models.IntegerField( )
+    cidade = models.CharField(max_length= 250 )
+    bairro = models.CharField(max_length= 250 )
+    def __init__(self, arg):
+        super(, self).__init__()
+        self.arg = arg
