8e66117b2cc6f9f22b989c9c0566552547c9dc0b
yago batista da silva <yagobatistasilva@gmail.com>
2017-12-16 17:33:36 -0200

    removido o alert do ajax.js

--- vendas/static/js/ajax.js
+++ vendas/static/js/ajax.js
@@ -6,7 +6,6 @@ $(document).ready(function(){
         var tab = $(this).parent().next().next();
         $.post(url,inputs,
             function(response){
-                alert(response);
                 $('#tot1').html($($.parseHTML(response)).filter("#tot1").html())
                 $('#tot2').html($($.parseHTML(response)).filter("#tot2").html())
                 tab.html($($.parseHTML(response)).filter("#sub-total").html());
